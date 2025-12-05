adad49e19c40cfe0fdd60d079973c47c4a6bae66
nymanerica88 <ervin.erica29@gmail.com>
2025-12-04 19:58:08 -0500

    Initial commit

---
+++ src/main.jsx
@@ -0,0 +1,13 @@
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router";
+import App from "./App.jsx";
+import { AuthProvider } from "./auth/AuthContext.jsx";
+import "./index.css";
+
+createRoot(document.getElementById("root")).render(
+  <AuthProvider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </AuthProvider>,
+);
