4ad3af7aa27ede0251f49fefc34527e419edfa4b
Erica Nyman <ervin.erica29@gmail.com>
2025-12-09 22:46:40 -0500

    feat: imported register CSS to main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,12 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router";
 import App from "./App.jsx";
 import { AuthProvider } from "./auth/AuthContext.jsx";
-import "./index.css";
+import "./CSSregister.css";
 
 createRoot(document.getElementById("root")).render(
   <AuthProvider>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </AuthProvider>,
+  </AuthProvider>
 );
