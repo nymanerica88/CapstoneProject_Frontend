48c1cdc75ce2a9bae75ab0e50bd3d0c3379fe150
Erica Nyman <ervin.erica29@gmail.com>
2025-12-09 22:40:05 -0500

    feat: added header, description, firstname, lastname, and email to the registration form

--- src/auth/Register.jsx
+++ src/auth/Register.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { Link, useNavigate } from "react-router";
-
 import { useAuth } from "./AuthContext";
+import "../CSSregister.css";
 
 /** A form that allows users to register for a new account */
 export default function Register() {
@@ -10,33 +10,56 @@ export default function Register() {
 
   const [error, setError] = useState(null);
 
-  const onRegister = async (formData) => {
+  const onRegister = async (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+    const first_name = formData.get("firstname");
+    const last_name = formData.get("lastname");
+    const email = formData.get("email");
     const username = formData.get("username");
     const password = formData.get("password");
     try {
-      await register({ username, password });
+      await register({ first_name, last_name, email, username, password });
       navigate("/");
     } catch (e) {
-      setError(e.message);
+      setError(e.message || "Registration failed");
     }
   };
 
   return (
-    <>
-      <h1>Register for an account</h1>
-      <form action={onRegister}>
+    <section className="register">
+      <h1>Welcome to Tab Tabulations!</h1>
+      <p>
+        Split bills with peers, family, and friends easily with out built in tab
+        calculator. Users can enter the bill total, choose a bill split type,
+        and quickly calculate what each individual owes on the group bill!
+      </p>
+      <h2>Register for an Account</h2>
+      <form onSubmit={onRegister}>
+        <label>
+          First Name
+          <input type="text" name="firstname" required />
+        </label>
+        <label>
+          Last Name
+          <input type="text" name="lastname" required />
+        </label>
+        <label>
+          Email
+          <input type="email" name="email" required />
+        </label>
         <label>
           Username
-          <input type="text" name="username" />
+          <input type="text" name="username" required />
         </label>
         <label>
           Password
           <input type="password" name="password" required />
         </label>
-        <button>Register</button>
+        <button type="submit">Register</button>
         {error && <output>{error}</output>}
       </form>
       <Link to="/login">Already have an account? Log in here.</Link>
-    </>
+    </section>
   );
 }
