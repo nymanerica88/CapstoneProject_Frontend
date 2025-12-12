950ed6615f546181ab5f5fb5f4eab3642dbdfe29
Erica Nyman <ervin.erica29@gmail.com>
2025-12-12 16:47:53 -0500

    feat: added the /profile/:guest_id route path

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router";
 import Layout from "./layout/Layout";
 import Login from "./auth/Login";
 import Register from "./auth/Register";
+import Profile from "./auth/Profile";
 
 export default function App() {
   return (
@@ -10,6 +11,7 @@ export default function App() {
         <Route index element={null} />
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/profile/:guest_id" element={<Profile />} />
       </Route>
     </Routes>
   );
