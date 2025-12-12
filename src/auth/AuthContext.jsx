0b9e9f7c1b6981e83bc1f0d1a6cfac257e34b89d
Erica Nyman <ervin.erica29@gmail.com>
2025-12-12 16:32:15 -0500

    feat: added state for user and guestId throughout; returned data.guest_id in the login function; corrected path typos (missing slashes)

--- src/auth/AuthContext.jsx
+++ src/auth/AuthContext.jsx
@@ -6,9 +6,17 @@ const AuthContext = createContext();
 
 export function AuthProvider({ children }) {
   const [token, setToken] = useState(sessionStorage.getItem("token"));
+  const [user, setUser] = useState(null);
+  const [guestId, setGuestId] = useState(null);
 
   useEffect(() => {
-    if (token) sessionStorage.setItem("token", token);
+    if (token) {
+      sessionStorage.setItem("token", token);
+    } else {
+      sessionStorage.removeItem("token");
+      setUser(null);
+      setGuestId(null);
+    }
   }, [token]);
 
   const register = async (credentials) => {
@@ -21,6 +29,15 @@ export function AuthProvider({ children }) {
     console.log(result);
     if (!response.ok) throw Error(result);
     setToken(result);
+
+    const retrieveGuestIdRegistration = await fetch(API + "/users/me", {
+      headers: { Authorization: `Bearer ${result}` },
+    });
+    const data = await retrieveGuestIdRegistration.json();
+    setUser(data.user);
+    setGuestId(data.guest_id ?? null);
+
+    return data.guest_id;
   };
 
   const login = async (credentials) => {
@@ -32,14 +49,33 @@ export function AuthProvider({ children }) {
     const result = await response.text();
     if (!response.ok) throw Error(result);
     setToken(result);
+
+    const retrieveGuestIdLogin = await fetch(API + "/users/me", {
+      headers: { Authorization: `Bearer ${result}` },
+    });
+    const data = await retrieveGuestIdLogin.json();
+    setUser(data.user);
+    setGuestId(data.guest_id ?? null);
+
+    return data.guest_id;
   };
 
   const logout = () => {
     setToken(null);
+    setUser(null);
+    setGuestId(null);
     sessionStorage.removeItem("token");
   };
 
-  const value = { token, register, login, logout };
+  const value = {
+    token,
+    user,
+    guestId,
+    isLoggedIn: !!token,
+    register,
+    login,
+    logout,
+  };
   return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
 }
 
