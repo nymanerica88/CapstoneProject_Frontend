9e6cee991038db483b0b143dbca9789f2b66058c
Erica Nyman <ervin.erica29@gmail.com>
2025-12-17 03:56:58 -0500

    feat: added return logic to render on the UI

--- src/auth/BillDetails.jsx
+++ src/auth/BillDetails.jsx
@@ -1,8 +1,9 @@
 import { useEffect, useState } from "react";
 import { useParams, useNavigate } from "react-router";
 import { useAuth } from "../auth/AuthContext";
+import "../CSSbilldetail.css";
 
-export default function BillDetail() {
+export default function BillDetails() {
   const { id } = useParams();
   const { token } = useAuth();
   const navigate = useNavigate();
@@ -43,7 +44,50 @@ export default function BillDetail() {
     }
     loadBill();
   }, [id, token]);
-}
 
-if (loading) return <p>Loading Bill...</p>;
-if (error) return <p>{error}</p>;
+  if (loading) return <p>Loading Bill...</p>;
+  if (error) return <p>{error}</p>;
+  if (!bill) return <p> Bill not found</p>;
+
+  return (
+    <>
+      <section className="entire-bill-detail-section">
+        <section className="bill-detail">
+          <h2>Bill #{bill.ref_num}</h2>
+
+          <p>
+            <strong>Total:</strong> ${Number(bill.total).toFixed(2)}
+          </p>
+          <p>
+            <strong>Split Type:</strong>
+            {bill.type}
+          </p>
+        </section>
+        <section className="bill-splits">
+          <h3>Bill Split</h3>
+          {splits.map((split) => (
+            <div key={split.id} className="split-row">
+              <span>{split.guest_name}</span>
+              <span>${Number(split.amount_owed).toFixed(2)}</span>
+            </div>
+          ))}
+        </section>
+        {bill.type === "per_item" && (
+          <section className="bill-items">
+            <h3>Items</h3>
+            {items.map((item, index) => (
+              <div key={index} className="item-row">
+                <span>{item.guest_name}</span>
+                <span>{item.item_name}</span>
+                <span>{item.quantity}</span>
+                <span>
+                  ${(Number(item.quantity) * Number(item.price)).toFixed(2)}
+                </span>
+              </div>
+            ))}
+          </section>
+        )}
+      </section>
+    </>
+  );
+}
